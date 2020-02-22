f5406b971729248b9e711badff5f428623e1471d
Johnny <jon.diaz413@gmail.com>
2020-02-22 18:31:31 -0500

    Added fleet dropdown and fleet change direction, also slowed down the fleet speed.

--- settings.py
+++ settings.py
@@ -12,7 +12,11 @@ class Settings:
         self.ship_speed = 1.5
 
         # Alien's speed
-        self.alien_speed = 1.0
+        self.alien_speed = 0.25
+        self.fleet_drop_speed = 10
+
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
 
         # Bullet settings
         self.bullet_speed = 1.0
