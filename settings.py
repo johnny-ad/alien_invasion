96c6611d674d7b1d9b679694c533ff7f12fcffe2
Johnny <jon.diaz413@gmail.com>
2020-02-24 23:19:22 -0500

    Added scoreboard.py, initialize the score in the game to be tracked.

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ class Settings:
         self.ship_limit = 3
 
         # Alien's speed
-        self.alien_speed = .50
+        self.alien_speed = .40
         self.fleet_drop_speed = 10
 
         # How quickly the game speeds up
@@ -35,7 +35,7 @@ class Settings:
         """Initialize settings that change throughout the game."""
         self.ship_speed = 1.5
         self.bullet_speed = 3.0
-        self.alien_speed = .50
+        self.alien_speed = .40
 
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
