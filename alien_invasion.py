5739a518bc62fee263806a22d7e0ffac7df96b2f
Johnny <jon.diaz413@gmail.com>
2020-02-26 10:11:27 -0500

    Finalized the scoreboard, amount of lives left and the top score; Project done until I figure out new ideas to improve the game. GET HYPED! >:^)

--- alien_invasion.py
+++ alien_invasion.py
@@ -74,6 +74,8 @@ class AlienInvasion:
             # Reset the game statistics.
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
@@ -128,11 +130,21 @@ class AlienInvasion:
         collisions = pygame.sprite.groupcollide(
             self.bullets, self.aliens, True, True)
 
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
+            self.sb.check_high_score()
+
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Increase level
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _update_aliens(self):
         """Update the positions of all aliens in the fleet."""
@@ -192,8 +204,9 @@ class AlienInvasion:
     def _ship_hit(self):
         """Respond to a ship being it by an alien."""
         if self.stats.ships_left > 0:
-            # Decrement ships_left.
+            # Decrement ships_left, and update scoreboard.
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
--- game_stats.py
+++ game_stats.py
@@ -6,6 +6,9 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
 
+        # A high score should never reset.
+        self.high_score = 0
+
         # Starts game in inactive state.
         self.game_active = False
 
@@ -13,3 +16,4 @@ class GameStats:
         """Initialize statistics that can change during the game."""
         self.ships_left = self.settings.ship_limit
         self.score = 0
+        self.level = 1
